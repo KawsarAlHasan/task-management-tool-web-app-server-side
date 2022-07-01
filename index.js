c73d3868f0c26dcbaa10ae8026cac47db598cafe
KawsarAlHasan <kawsaralhasan.420@gmail.com>
2022-07-02 01:31:32 +0600

    added post todo

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -9,12 +10,31 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mm223.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    console.log('connected')
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run() {
+    try {
+        await client.connect();
+        const todoCollection = client.db('taskManagement').collection('todo');
+
+        app.get('/todo', async (req, res) => {
+            const query = {};
+            const cursor = todoCollection.find(query);
+            const todos = await cursor.toArray();
+            res.send(todos);
+        });
+
+        // todo post
+        app.post('/addTodo', async (req, res) => {
+            const newParts = req.body;
+            const result = await todoCollection.insertOne(newParts);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
